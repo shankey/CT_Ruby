9d5054c05bef08499f5c09d47995127a0057d6e3
Aditya nema <shankey.adi@gmail.com>
2015-11-24 13:03:34 +0000

    2o stories and title

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,7 +1,13 @@
 module PlacesHelper
     
-    def getPlaceUrl(params)
+    def get_place_url(params)
         url = '/places/' + params[:id]
+        puts url
+        return url
+    end
+    
+    def get_place_title_url(params)
+        url = '/places/' + params[:id] + '_title'
         puts url
         return url
     end
