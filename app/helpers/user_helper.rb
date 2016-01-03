441d4d4d970e101fbdc6431f6b2180bd47ba09db
Aditya nema <shankey.adi@gmail.com>
2016-01-03 13:27:57 +0000

    default picture

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -2,4 +2,8 @@ module UserHelper
   def get_profile_pic(user)
     return user.profile_pictures || image_url('user-default.png')
   end
+  
+  def get_tile_image(story)
+    return story.image || "/images/Gaurav_Jaipur_images/jal_mahal.jpg"
+  end
 end
